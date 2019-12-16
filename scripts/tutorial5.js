d44a48902b44ad9cf71df1b50af44fd16febe994
kmskpraveen <bgkmskpraveenmb@gmail.com>
2019-12-16 15:00:00 +0530

    5 - Var Hoisting & Functional Scope

---
+++ scripts/tutorial5.js
@@ -0,0 +1,18 @@
+// Hoisting in Javascript is that all the var declarations 
+// is moved to top of the function. So, the below function is valid.
+// Also var declaration has functional scope. It doesn't matter if you declare it in if block or so
+// So, var greet = "Hello Chandler" and deletion of var greet; is valid!
+// Because of Hoisting and functional scope, var is tricky in JS
+// So, they introduced 'let' IN ES6
+function greetPerson(name) {
+    if (name === "Chandler") {
+        var greet = "Hello Chandler";
+    }
+    else {
+        var greet = "Hi there";
+    }
+    // var greet;
+    console.log(greet);
+}
+greetPerson("Chandler");
+//# sourceMappingURL=tutorial5.js.map
