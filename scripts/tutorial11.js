e405889608d9d2c35dad7681e3181760248d572a
kmskpraveen <bgkmskpraveenmb@gmail.com>
2019-12-17 15:22:26 +0530

    11 - lexical 'this'

---
+++ scripts/tutorial11.js
@@ -0,0 +1,30 @@
+var employee = {
+    id: 1,
+    greet: function () {
+        setTimeout(function () { console.log(this.id); }, 1000);
+    }
+};
+employee.greet(); // undefined.. because
+// When you have a function it is going to create its own context
+// of 'this' keyword ..
+// A small work around to the above problem
+var employee1 = {
+    id: 1,
+    greet: function () {
+        var self = this; // saving context of 'this' keyword within this function 
+        setTimeout(function () { console.log(self.id); }, 1000);
+    }
+};
+employee1.greet(); // 1
+// Now, with arrow functions it is much easier..
+var employee2 = {
+    id: 1,
+    greet: function () {
+        var _this = this;
+        setTimeout(function () { console.log(_this.id); }, 1000);
+    }
+};
+employee2.greet(); // 1, because, whenever you use an arrow function,
+// it is not going to create its own 'this'. 
+// So, 'this' in arrow function = parent's 'this'
+//# sourceMappingURL=tutorial11.js.map
